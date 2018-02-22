fe30eb98dbb464fa7262a8c0979d34898d96a545
Miguel Panelo <miguelrpanelo@gmail.com>
2018-02-22 22:54:41 +0800

    Added test for StateMachine

--- kaskade/src/test/kotlin/io/gumil/kaskade/StateMachineTest.kt
+++ kaskade/src/test/kotlin/io/gumil/kaskade/StateMachineTest.kt
@@ -18,12 +18,36 @@ package io.gumil.kaskade
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 internal class StateMachineTest {
 
+    private val stateMachine = StateMachine<TestState, TestAction, TestResult>(TestState.State1).apply {
+        addAction(TestAction.Action1, DeferredValue(TestResult.Result1()))
+        addAction(TestAction.Action2, DeferredValue(TestResult.Result2()))
+    }
+
     @Test
-    fun test() {
-        assertEquals(2, 1 + 1)
+    fun testShouldThrowException() {
+        assertFailsWith(IllegalStateException::class) {
+            stateMachine.processAction(TestAction.Action3)
+        }
+    }
+
+    @Test
+    fun testShouldEmitState1() {
+        stateMachine.processAction(TestAction.Action1)
+        stateMachine.onStateChanged = {
+            assertEquals(TestState.State1, it)
+        }
+    }
+
+    @Test
+    fun testShouldEmitState2() {
+        stateMachine.processAction(TestAction.Action2)
+        stateMachine.onStateChanged = {
+            assertEquals(TestState.State2, it)
+        }
     }
 
 }
