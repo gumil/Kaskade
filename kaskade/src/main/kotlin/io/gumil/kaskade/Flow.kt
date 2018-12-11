7ddd3d1efaec5bc9f9f34ee8f7e0c08a1f69cee0
Miguel Panelo <miguelrpanelo@gmail.com>
2018-12-11 22:38:23 +0100

    Implement DamFlow that persists last emitted state

--- kaskade/src/main/kotlin/io/gumil/kaskade/Flow.kt
+++ kaskade/src/main/kotlin/io/gumil/kaskade/Flow.kt
@@ -1,24 +1,51 @@
 package io.gumil.kaskade
 
-class Flow<T> {
+open class Flow<T> {
 
     private var subscription: ((T) -> Unit)? = null
 
-    fun subscribe(subscription: (T) -> Unit) {
+    open fun subscribe(subscription: (T) -> Unit) {
         this.subscription = subscription
     }
 
-    fun sendValue(value: T) {
+    open fun sendValue(value: T) {
         subscription?.invoke(value)
     }
 
-    fun unsubscribe() {
+    open fun unsubscribe() {
         subscription = null
     }
 }
 
+class DamFlow<T> : Flow<T>() {
+
+    private var savedValue: T? = null
+
+    override fun subscribe(subscription: (T) -> Unit) {
+        super.subscribe(subscription)
+        savedValue?.let { sendValue(it) }
+    }
+
+    override fun sendValue(value: T) {
+        savedValue = value
+        super.sendValue(value)
+    }
+
+    override fun unsubscribe() {
+        super.unsubscribe()
+        savedValue = null
+    }
+}
+
 fun <S : State, A : Action> Kaskade<S, A>.stateFlow(): Flow<S> {
-    val state = Flow<S>()
-    onStateChanged = { state.sendValue(it) }
-    return state
+    return createFlow(Flow())
+}
+
+fun <S : State, A : Action> Kaskade<S, A>.stateDamFlow(): Flow<S> {
+    return createFlow(DamFlow())
+}
+
+private fun <A : Action, S : State> Kaskade<S, A>.createFlow(flow: Flow<S>): Flow<S> {
+    onStateChanged = { flow.sendValue(it) }
+    return flow
 }
