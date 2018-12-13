cc51bb067ea6737d12d6f03153e9ecaed6a410f7
Miguel Panelo <miguelrpanelo@gmail.com>
2018-12-13 23:24:41 +0100

    Add tests to Flow

--- kaskade/src/main/kotlin/io/gumil/kaskade/flow/Flow.kt
+++ kaskade/src/main/kotlin/io/gumil/kaskade/flow/Flow.kt
@@ -2,22 +2,24 @@ package io.gumil.kaskade.flow
 
 open class MutableFlow<T>: Flow<T> {
 
-    override var subscription: ((T) -> Unit)? = null
+    protected open var subscription: ((T) -> Unit)? = null
 
     open fun sendValue(value: T) {
         subscription?.invoke(value)
+    }
+
+    override fun subscribe(subscription: (T) -> Unit) {
+        this.subscription = subscription
+    }
+
+    override fun unsubscribe() {
+        subscription = null
     }
 }
 
 interface Flow<T> {
 
-    var subscription: ((T) -> Unit)?
+    fun subscribe(subscription: (T) -> Unit)
 
-    fun subscribe(subscription: (T) -> Unit) {
-        this.subscription = subscription
-    }
-
-    fun unsubscribe() {
-        subscription = null
-    }
+    fun unsubscribe()
 }
