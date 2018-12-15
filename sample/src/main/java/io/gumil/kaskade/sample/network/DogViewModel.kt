ed601b7eb6355164c15868ad86b22d0dec96d306
Miguel Panelo <miguelrpanelo@gmail.com>
2018-12-15 20:41:57 +0100

    Extracted dispatcher for testing

--- sample/src/main/java/io/gumil/kaskade/sample/network/DogViewModel.kt
+++ sample/src/main/java/io/gumil/kaskade/sample/network/DogViewModel.kt
@@ -6,19 +6,21 @@ import io.gumil.kaskade.Action
 import io.gumil.kaskade.Kaskade
 import io.gumil.kaskade.State
 import io.gumil.kaskade.livedata.stateDamLiveData
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 
 internal class DogViewModel(
-        private val dogApi: RandomDogApi
+        private val dogApi: RandomDogApi,
+        dispatcher: CoroutineDispatcher = Dispatchers.Main
 ): ViewModel() {
 
     constructor(): this(ApiFactory.create())
 
     private val job = Job()
 
-    private val uiScope = CoroutineScope(Dispatchers.Main + job)
+    private val uiScope = CoroutineScope(dispatcher + job)
 
     private val kaskade = Kaskade.create<DogState, DogAction>(DogState.OnLoaded("")) {
         on<DogAction.Refresh> {
