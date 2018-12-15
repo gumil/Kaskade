8205ddd98588a620a38417851630b644d47330f7
Miguel Panelo <miguelrpanelo@gmail.com>
2018-12-15 22:04:30 +0100

    Move coroutines to a separate module

---
+++ kaskade-coroutines/src/main/kotlin/com/gumil/kaskade/coroutines/ScopedReducer.kt
@@ -0,0 +1,22 @@
+package com.gumil.kaskade.coroutines
+
+import io.gumil.kaskade.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+data class ScopedReducer<ACTION: Action, STATE: State>(
+        private val transformerFunction: suspend ActionState<ACTION, STATE>.() -> STATE,
+        private val coroutineScope: CoroutineScope
+) : Reducer<ACTION, STATE> {
+
+    override fun invoke(action: ACTION, state: STATE, onStateChanged: (state: STATE) -> Unit) {
+        coroutineScope.launch { onStateChanged(transformerFunction(ActionState(action, state))) }
+    }
+}
+
+inline fun <S : State, A : Action, reified T : A> Kaskade.Builder<A, S>.on(
+        scope: CoroutineScope,
+        noinline transformer: suspend ActionState<T, S>.() -> S
+) {
+    on(T::class, ScopedReducer(transformer, scope))
+}
