22e209830cfdccceb20eb696f38cf4950c98333a
Miguel Panelo <miguelrpanelo@gmail.com>
2018-12-15 22:38:25 +0100

    Refactor creation of coroutine actions

--- kaskade-coroutines/src/main/kotlin/com/gumil/kaskade/coroutines/ScopedReducer.kt
+++ kaskade-coroutines/src/main/kotlin/com/gumil/kaskade/coroutines/ScopedReducer.kt
@@ -1,22 +1,18 @@
 package com.gumil.kaskade.coroutines
 
-import io.gumil.kaskade.*
+import io.gumil.kaskade.Action
+import io.gumil.kaskade.ActionState
+import io.gumil.kaskade.Reducer
+import io.gumil.kaskade.State
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 data class ScopedReducer<ACTION: Action, STATE: State>(
-        private val transformerFunction: suspend ActionState<ACTION, STATE>.() -> STATE,
-        private val coroutineScope: CoroutineScope
+        private val coroutineScope: CoroutineScope,
+        private val transformerFunction: suspend ActionState<ACTION, STATE>.() -> STATE
 ) : Reducer<ACTION, STATE> {
 
     override fun invoke(action: ACTION, state: STATE, onStateChanged: (state: STATE) -> Unit) {
         coroutineScope.launch { onStateChanged(transformerFunction(ActionState(action, state))) }
     }
-}
-
-inline fun <A : Action, S : State, reified T : A> Kaskade.Builder<A, S>.on(
-        scope: CoroutineScope,
-        noinline transformer: suspend ActionState<T, S>.() -> S
-) {
-    on(T::class, ScopedReducer(transformer, scope))
 }
