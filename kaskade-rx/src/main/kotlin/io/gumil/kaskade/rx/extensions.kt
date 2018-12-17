c4fc29d23deabb153f04866416cf6bb9739929e4
Miguel Panelo <miguelrpanelo@gmail.com>
2018-12-17 23:47:13 +0100

    Add stateObservable extension

---
+++ kaskade-rx/src/main/kotlin/io/gumil/kaskade/rx/extensions.kt
@@ -0,0 +1,18 @@
+package io.gumil.kaskade.rx
+
+import io.gumil.kaskade.Action
+import io.gumil.kaskade.Kaskade
+import io.gumil.kaskade.State
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+
+fun <A : Action, S : State> Kaskade.Builder<A, S>.rx(builder: RxKaskadeBuilder<A, S>.() -> Unit) {
+    builder(RxKaskadeBuilder(this))
+}
+
+fun <A : Action, S : State> Kaskade<A, S>.stateObservable(): Observable<S> =
+        PublishSubject.create<S>().apply {
+            onStateChanged = {
+                onNext(it)
+            }
+        }
