c102c22d35916db460fd8ca19e33085805821be7
Miguel Panelo <miguelrpanelo@gmail.com>
2018-12-19 23:16:07 +0100

    Mock invoke in tests

--- sample-android/src/test/kotlin/io/gumil/kaskade/sample/todo/TodoKaskadeTest.kt
+++ sample-android/src/test/kotlin/io/gumil/kaskade/sample/todo/TodoKaskadeTest.kt
@@ -4,54 +4,61 @@ import io.gumil.kaskade.sample.todo.data.TodoItem
 import io.gumil.kaskade.sample.todo.data.TodoRepository
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Before
 import org.junit.Test
-import kotlin.test.assertEquals
 
 class TodoKaskadeTest {
 
     private val repository = mockk<TodoRepository>(relaxUnitFun = true)
 
+    private val subscriber = mockk<(TodoState) -> Unit>(relaxed = true)
+
     private val todoKaskade = TodoKaskade(repository)
 
     @Before
-    fun `should emit initial state`() {
-        todoKaskade.state.subscribe {
-            assertEquals(TodoState.OnLoaded(listOf()), it)
-        }
+    fun setUp() {
+        every { subscriber.invoke(any()) } returns Unit
     }
 
     @Test
     fun `Action Refresh`() {
         every { repository.getToDoItems() } returns emptyList()
 
-        todoKaskade.state.subscribe { assertEquals(TodoState.OnLoaded(emptyList()), it) }
+        todoKaskade.state.subscribe(subscriber)
 
         todoKaskade.process(TodoAction.Refresh)
+        verify { subscriber.invoke(TodoState.OnLoaded(emptyList())) }
     }
 
     @Test
     fun `Action Delete`() {
-        todoKaskade.state.subscribe { assertEquals(TodoState.OnDeleted(1), it) }
+        todoKaskade.state.subscribe(subscriber)
 
         todoKaskade.process(TodoAction.Delete(1, TodoItem(1, "test", false)))
+
+        verify { subscriber.invoke(TodoState.OnDeleted(1)) }
     }
 
     @Test
     fun `Action Add`() {
         val todoItem = TodoItem(1, "test", false)
 
-        todoKaskade.state.subscribe { assertEquals(TodoState.OnAdded(todoItem), it) }
+        todoKaskade.state.subscribe(subscriber)
 
         todoKaskade.process(TodoAction.Add(todoItem))
+
+        verify { subscriber.invoke(TodoState.OnAdded(todoItem)) }
     }
 
     @Test
     fun `Action Update`() {
         val todoItem = TodoItem(1, "test", false)
 
-        todoKaskade.state.subscribe { assertEquals(TodoState.OnUpdated(1, todoItem), it) }
+        todoKaskade.state.subscribe(subscriber)
 
         todoKaskade.process(TodoAction.Update(1, todoItem))
+
+        verify { subscriber.invoke(TodoState.OnUpdated(1, todoItem)) }
     }
 }
