d3199ae6576977be73e89404c94e4002ccda7abc
Miguel Panelo <miguelrpanelo@gmail.com>
2018-12-19 22:48:18 +0100

    Add music player console app

--- sample-kotlin/src/main/kotlin/io/gumil/kaskade/sample/Main.kt
+++ sample-kotlin/src/main/kotlin/io/gumil/kaskade/sample/Main.kt
@@ -1,5 +1,45 @@
 package io.gumil.kaskade.sample
 
+import io.gumil.kaskade.stateFlow
+
 fun main(args: Array<String>) {
-    println("hello world")
+    println("""
+
+        ########################
+        # KASKADE MUSIC PLAYER #
+        ########################
+
+        ########################
+        # controls:            #
+        #  z = stop            #
+        #  x = pause/play      #
+        #  c = previous        #
+        #  v = next            #
+        #  q = exit            #
+        ########################
+
+    """.trimIndent())
+
+    val kaskade = MusicPlayerKaskade().kaskade
+
+    kaskade.stateFlow().subscribe {
+        when (it) {
+            is PlayerState.Playing -> println("Now Playing: ${it.music}")
+            PlayerState.Stopped -> println("STOPPED")
+            PlayerState.Paused -> println("Paused")
+        }
+    }
+
+    loop@while(true) {
+        val action = readLine()?.get(0) ?: 'q'
+
+        when (action) {
+            'z' -> kaskade.process(PlayerAction.Stop)
+            'x' -> kaskade.process(PlayerAction.PausePlay)
+            'c' -> kaskade.process(PlayerAction.Previous)
+            'v' -> kaskade.process(PlayerAction.Next)
+            else -> break@loop
+        }
+    }
 }
+
