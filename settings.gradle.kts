267e8cda2a1ce3ddf1eca3240615415d57e1e7cf
Miguel Panelo <miguelrpanelo@gmail.com>
2019-01-14 12:58:00 +0100

    Rename build.gradle files

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+include(
+    ":kaskade",
+    ":kaskade-rx",
+    ":kaskade-livedata",
+    ":kaskade-coroutines",
+    ":sample-android",
+    ":sample-kotlin"
+)
+
+rootProject.children.forEach {
+    it.buildFileName = "${it.name}.gradle"
+}
