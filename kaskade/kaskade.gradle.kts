86a7dcf009c14137ffea6d542cbee3c752fbe19b
Miguel Panelo <miguelrpanelo@gmail.com>
2019-03-11 22:44:21 +0100

    Add iosTest task

--- kaskade/kaskade.gradle.kts
+++ kaskade/kaskade.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform")
     id("com.github.dcendents.android-maven")
@@ -44,6 +46,19 @@ kotlin {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
             }
+        }
+    }
+}
+
+tasks.register("iosTest") {
+    dependsOn("iosX64TestBinaries")
+    doLast {
+        val binary = (kotlin.targets["iosX64"] as KotlinNativeTarget)
+            .compilations["test"]
+            .getBinary("EXECUTABLE", "DEBUG")
+
+        exec {
+            commandLine("xcrun", "simctl", "spawn", "iPhone XR", binary.absolutePath)
         }
     }
 }
