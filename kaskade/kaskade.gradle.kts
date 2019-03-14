fc7ec857bf5d949c8bd1678587b9bdec143c6cf6
Miguel Panelo <miguelrpanelo@gmail.com>
2019-03-14 21:10:51 +0100

    Add js test tasks

--- kaskade/kaskade.gradle.kts
+++ kaskade/kaskade.gradle.kts
@@ -1,19 +1,41 @@
+import com.moowork.gradle.node.npm.NpmTask
+import com.moowork.gradle.node.task.NodeTask
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilationToRunnableFiles
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     kotlin("multiplatform")
+    id("com.moowork.node") version "1.2.0"
     id("com.github.dcendents.android-maven")
 }
 
 kotlin {
     jvm()
-    js()
+    js {
+        configure(listOf(compilations["main"], compilations["test"])) {
+            tasks.getByName(compileKotlinTaskName) {
+                kotlinOptions {
+                    metaInfo = true
+                    sourceMap = true
+                    sourceMapEmbedSources = "always"
+                    moduleKind = "umd"
+                }
+            }
+        }
+    }
     iosX64()
 
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(kotlin("stdlib-common"))
+                api(kotlin("stdlib-common"))
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                api(kotlin("test-common"))
+                api(kotlin("test-annotations-common"))
             }
         }
 
@@ -40,13 +62,6 @@ kotlin {
                 implementation(kotlin("test-js"))
             }
         }
-
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
     }
 }
 
@@ -62,3 +77,42 @@ tasks.register("iosTest") {
         }
     }
 }
+
+node {
+    version = "10.9.0"
+    download = true
+    nodeModulesDir = file("$buildDir/npm/")
+}
+
+tasks.register<NpmTask>("installMocha") {
+    setWorkingDir(node.nodeModulesDir)
+    setArgs(listOf("install", "mocha@6.0.0"))
+}
+
+val jsCompilations = kotlin.targets["js"].compilations
+
+tasks.register("populateNodeModules") {
+    doLast {
+        copy {
+        from("$buildDir/npm/node_modules")
+        from(jsCompilations["main"].output.allOutputs)
+        (jsCompilations["test"] as KotlinCompilationToRunnableFiles).runtimeDependencyFiles.forEach {
+            if (it.exists() && !it.isDirectory) {
+                from(zipTree(it.absolutePath).matching { include("*.js") })
+            }
+        }
+        into("$buildDir/node_modules")
+        }
+    }
+}
+
+tasks.register<NodeTask>("runMocha") {
+    dependsOn("compileTestKotlinJs", "installMocha", "populateNodeModules")
+    setScript(file("$buildDir/npm/node_modules/.bin/mocha"))
+    setArgs(listOf(
+        "--timeout", "15000",
+        relativePath(jsCompilations["test"].output.allOutputs.first()) + "/${project.name}_test.js"
+    ))
+}
+
+tasks["jsTest"].dependsOn("runMocha")
