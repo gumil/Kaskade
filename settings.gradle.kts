fecef7c5d324086cb979b094b8713600c588c295
Miguel Panelo <miguelrpanelo@gmail.com>
2019-08-18 13:30:40 +0200

    Exclude projects during osx build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,20 @@
 include(":core")
-include(":rx")
-include(":livedata")
 include(":coroutines")
-include(":app")
-include(":console")
 
 project(":core").projectDir = file("kaskade/core")
-project(":rx").projectDir = file("kaskade/rx")
-project(":livedata").projectDir = file("kaskade/livedata")
 project(":coroutines").projectDir = file("kaskade/coroutines")
-project(":app").projectDir = file("sample/app")
-project(":console").projectDir = file("sample/console")
+
+if (System.getenv("TRAVIS_OS_NAME") != "osx") {
+    include(":rx")
+    include(":livedata")
+    include(":app")
+    include(":console")
+
+    project(":rx").projectDir = file("kaskade/rx")
+    project(":livedata").projectDir = file("kaskade/livedata")
+    project(":app").projectDir = file("sample/app")
+    project(":console").projectDir = file("sample/console")
+}
 
 rootProject.children.forEach {
     it.buildFileName = "${it.name}.gradle.kts"
