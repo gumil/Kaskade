a74c6fbf2ce55f8ad874b2ae7625e42d64d99686
Miguel Panelo <miguelrpanelo@gmail.com>
2019-08-24 13:21:06 +0200

    Update AGP to 3.5.0

--- buildSrc/src/main/kotlin/deps.kt
+++ buildSrc/src/main/kotlin/deps.kt
@@ -23,7 +23,7 @@ object deps {
             const val targetSdkVersion = 29
         }
 
-        const val classpath = "com.android.tools.build:gradle:3.4.2"
+        const val classpath = "com.android.tools.build:gradle:3.5.0"
 
         object lifecycle {
             const val livedata = "androidx.lifecycle:lifecycle-livedata:${versions.lifecycle}"
