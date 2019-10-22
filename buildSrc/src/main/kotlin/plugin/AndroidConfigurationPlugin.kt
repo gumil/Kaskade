7724e065a15ed1737404e16a8ba917f84732cdf4
Miguel Panelo <miguelrpanelo@gmail.com>
2019-10-22 18:23:28 +0200

    Extract common Android configuration to plugin

---
+++ buildSrc/src/main/kotlin/plugin/AndroidConfigurationPlugin.kt
@@ -0,0 +1,34 @@
+package plugin
+
+import com.android.build.gradle.BaseExtension
+import deps
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidConfigurationPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        project.configureAndroid()
+    }
+
+    private fun Project.configureAndroid() = this.extensions.getByType<BaseExtension>().run {
+        compileSdkVersion(deps.android.build.targetSdkVersion)
+        buildToolsVersion(deps.android.build.buildToolsVersion)
+        defaultConfig {
+            minSdkVersion(deps.android.build.minSdkVersion)
+            targetSdkVersion(deps.android.build.targetSdkVersion)
+        }
+
+        sourceSets {
+            getByName("main").java.srcDir("src/main/kotlin")
+            getByName("test").java.srcDir("src/test/kotlin")
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+}
