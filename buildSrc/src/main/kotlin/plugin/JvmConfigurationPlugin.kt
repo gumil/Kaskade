5704676de5785cd325b41c7469a47e07a091cc38
Miguel Panelo <miguelrpanelo@gmail.com>
2019-11-02 22:18:12 +0100

    Add JvmConfigurationPlugin

---
+++ buildSrc/src/main/kotlin/plugin/JvmConfigurationPlugin.kt
@@ -0,0 +1,35 @@
+package plugin
+
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.kotlin.dsl.getByType
+
+class JvmConfigurationPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        project.configureJvm()
+    }
+}
+
+class JvmLibraryConfigurationPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        project.configureJvm()
+        project.configureMavenJvm()
+    }
+}
+
+private fun Project.configureJvm() {
+    project.extensions.getByType<SourceSetContainer>().run {
+        getByName("main").java.srcDir("src/main/kotlin")
+        getByName("test").java.srcDir("src/test/kotlin")
+    }
+
+    project.extensions.getByType<JavaPluginExtension>().run {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
