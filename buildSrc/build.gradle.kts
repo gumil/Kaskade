42653a8b95d2fcbeb4a572aac7a7888c3154933f
Miguel Panelo <miguelrpanelo@gmail.com>
2020-05-02 12:01:04 +0200

    Update plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,12 +16,12 @@ plugins {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:3.5.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
-    implementation("com.github.node-gradle:gradle-node-plugin:2.2.0")
-    implementation("org.codehaus.groovy:groovy:2.5.7")
-    implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.1.1")
+    implementation("com.android.tools.build:gradle:3.6.3")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    implementation("com.github.node-gradle:gradle-node-plugin:2.2.3")
+    implementation("org.codehaus.groovy:groovy:3.0.3")
+    implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.8.0")
 
     implementation(gradleApi())
 }
