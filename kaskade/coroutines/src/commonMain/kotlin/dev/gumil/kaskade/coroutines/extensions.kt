29abbf62370ab756696d89850506c1d7a8a92f4a
Miguel Panelo <miguelrpanelo@gmail.com>
2020-06-25 23:11:14 +0200

    Add support to coroutines stateFlow
    This is experimental and DamStateFlow is not working
    similarly with DamLiveData or DamEmitter.

--- kaskade/coroutines/src/commonMain/kotlin/dev/gumil/kaskade/coroutines/extensions.kt
+++ kaskade/coroutines/src/commonMain/kotlin/dev/gumil/kaskade/coroutines/extensions.kt
@@ -4,6 +4,9 @@ import dev.gumil.kaskade.Action
 import dev.gumil.kaskade.Kaskade
 import dev.gumil.kaskade.State
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 /**
  * Extension function for building the coroutines DSL with independent scopes.
@@ -25,4 +28,30 @@ fun <A : Action, S : State> Kaskade.Builder<A, S>.coroutines(
     builder: CoroutinesScopedKaskadeBuilder<A, S>.() -> Unit
 ) {
     builder(CoroutinesScopedKaskadeBuilder(scope, this))
+}
+
+/**
+ * @return [StateFlow] of states from Kaskade.
+ */
+@ExperimentalCoroutinesApi
+fun <A : Action, S : State> Kaskade<A, S>.stateFlow(): StateFlow<S> =
+    createStateFlow(MutableStateFlow(initialState))
+
+/**
+ * Creates a [DamStateFlow] that saves emissions.
+ *
+ * @return [StateFlow] of states from Kaskade.
+ */
+@ExperimentalCoroutinesApi
+fun <A : Action, S : State> Kaskade<A, S>.stateDamFlow(): DamStateFlow<S> =
+    createStateFlow(DamStateFlow(initialState))
+
+@ExperimentalCoroutinesApi
+private fun <A : Action, S : State, F : MutableStateFlow<S>> Kaskade<A, S>.createStateFlow(
+    state: F
+): F {
+    onStateChanged = {
+        state.value = it
+    }
+    return state
 }
---
+++ kaskade/coroutines/src/commonTest/kotlin/dev/gumil/kaskade/coroutines/StateFlowTest.kt
@@ -0,0 +1,138 @@
+package dev.gumil.kaskade.coroutines
+
+import dev.gumil.kaskade.Kaskade
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.yield
+import kotlin.test.Test
+
+@ExperimentalCoroutinesApi
+internal class StateFlowTest {
+
+    private val kaskade = Kaskade.create<TestAction, TestState>(TestState.State1) {
+        on<TestAction.Action1> {
+            TestState.State1
+        }
+        on<TestAction.Action2> {
+            TestState.State2
+        }
+    }
+
+    @Test
+    fun stateFlow_only_emits_initial_state() {
+        val verifier = Verifier<TestState>()
+        val numberOfStateChanges = 1
+
+        runTest({
+            kaskade.stateFlow()
+                .take(numberOfStateChanges)
+                .verifyCollect(this, verifier.function)
+                .join()
+        }) {
+            verifier.verifyInvokedWithValue(TestState.State1)
+        }
+    }
+
+    @Test
+    fun set_value_on_stateflow_should_only_emit_value_that_was_set() {
+        val verifier = Verifier<TestState>()
+        val numberOfStateChanges = 1
+
+        runTest({
+            val stateFlow = kaskade.stateFlow() as MutableStateFlow
+            val job = stateFlow
+                .take(numberOfStateChanges)
+                .verifyCollect(this, verifier.function)
+
+            stateFlow.value = TestState.State2
+
+            job.join()
+        }) {
+            verifier.verifyInvokedWithValue(TestState.State2)
+        }
+    }
+
+    @Test
+    fun stateFlow_invoke_latest_emitted_value_before_observing() {
+        val verifier = Verifier<TestState>()
+        val numberOfStateChanges = 2
+
+        runTest({
+            val stateFlow = kaskade.stateFlow() as MutableStateFlow
+            stateFlow.value = TestState.State1
+            stateFlow.value = TestState.State2
+            stateFlow.value = TestState.State1
+
+            stateFlow
+                .take(numberOfStateChanges)
+                .verifyCollect(this, verifier.function)
+
+            yield() // Initial value
+
+            stateFlow.value = TestState.State2
+        }) {
+            verifier.verifyInvokedWithValue(TestState.State1)
+            verifier.verifyInvokedWithValue(TestState.State2)
+        }
+    }
+
+    @Test
+    fun stateFlow_should_not_invoke_anything_after_state_flow_is_done() {
+        val numberOfStateChanges = 1
+        val verifier = Verifier<TestState>()
+
+        runTest({
+            val stateFlow = kaskade.stateFlow() as MutableStateFlow
+            stateFlow
+                .take(numberOfStateChanges)
+                .verifyCollect(this, verifier.function)
+            yield() // Initial value
+            stateFlow.value = TestState.State2
+        }) {
+            verifier.verifyInvokedWithValue(TestState.State1, 1)
+            verifier.verifyInvokedWithValue(TestState.State2, 0)
+        }
+    }
+
+    @Test
+    fun stateFlow_should_invoke_last_emitted_after_first_collect_is_done() {
+        val numberOfStateChanges = 1
+        val verifier = Verifier<TestState>()
+
+        runTest({
+            val stateFlow = kaskade.stateFlow() as MutableStateFlow
+            stateFlow
+                .take(numberOfStateChanges)
+                .verifyCollect(this, verifier.function)
+            yield() // Initial value
+            stateFlow.value = TestState.State2
+            stateFlow
+                .take(numberOfStateChanges)
+                .verifyCollect(this, verifier.function)
+            yield()
+        }) {
+            verifier.verifyInvokedWithValue(TestState.State1, 1)
+            verifier.verifyInvokedWithValue(TestState.State2, 1)
+        }
+    }
+
+    @Test
+    fun should_emit_initial_and_processed_state() {
+        val verifier = Verifier<TestState>()
+        val numberOfStateChanges = 2
+
+        runTest({
+            val stateFlow = kaskade.stateFlow()
+            stateFlow
+                .take(numberOfStateChanges)
+                .verifyCollect(this, verifier.function)
+            yield()
+            kaskade.process(TestAction.Action2)
+            yield()
+        }) {
+            verifier.verifyInvokedWithValue(TestState.State1)
+            verifier.verifyInvokedWithValue(TestState.State2)
+        }
+    }
+}
